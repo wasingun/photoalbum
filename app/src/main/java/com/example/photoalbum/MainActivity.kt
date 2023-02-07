2ffd4c813eadccb6460ed11f8ccd56ad839bb74c
wasingun <wasingun00@gmail.com>
2023-02-08 03:05:50 +0900

    1번 고양이 구현

--- app/src/main/java/com/example/photoalbum/MainActivity.kt
+++ app/src/main/java/com/example/photoalbum/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.photoalbum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import kotlinx.coroutines.joinAll
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val catBtn: ImageButton = findViewById(R.id.imageButton1)
+
+        catBtn.setOnClickListener {
+            val cat1 = Intent(this, cat1::class.java)
+            startActivity(cat1)
+        }
     }
 }
---
+++ app/src/main/java/com/example/photoalbum/cat1.kt
@@ -0,0 +1,11 @@
+package com.example.photoalbum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cat1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cat)
+    }
+}
